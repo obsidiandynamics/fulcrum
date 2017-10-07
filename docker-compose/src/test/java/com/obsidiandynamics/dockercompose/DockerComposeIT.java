793d11771ddb22454603923c309a82233156678d
Emil Koutanov <ekoutanov@gmail.com>
2017-10-08 09:10:20 +1100

    Install check added

--- docker-compose/src/test/java/com/obsidiandynamics/dockercompose/DockerComposeIT.java
+++ docker-compose/src/test/java/com/obsidiandynamics/dockercompose/DockerComposeIT.java
@@ -5,12 +5,20 @@ import org.junit.*;
 import com.obsidiandynamics.shell.*;
 
 public final class DockerComposeIT {
+  @BeforeClass
+  public static void beforeClass() {
+    new DockerCompose()
+    .withShell(new BourneShell().withPath("/usr/local/bin"))
+    .checkInstalled();
+  }
+  
   @Test
-  public void test() throws DockerComposeException {
+  public void testUpDown() throws DockerComposeException {
     final DockerCompose compose = new DockerCompose()
         .withShell(new BourneShell().withPath("/usr/local/bin"))
         .withComposeFile("stacks/java8/docker-compose.yaml")
         .withProject("test-docker-compose")
+        .withEcho(true)
         .withSink(System.out::println);
     
     compose.up();
