48f951045babbdc340ffd1bc447934d46e57de15
Emil Koutanov <ekoutanov@gmail.com>
2017-10-14 06:55:30 +1100

    Javadocs

--- version/src/main/java/com/obsidiandynamics/version/AppVersion.java
+++ version/src/main/java/com/obsidiandynamics/version/AppVersion.java
@@ -3,12 +3,29 @@ package com.obsidiandynamics.version;
 import java.io.*;
 import java.net.*;
 
+/**
+ *  Enables an application to read a version and build number stored in the
+ *  application's classpath.<p>
+ *  
+ *  These artefacts are read by convention; for an application {@code foo}, the
+ *  version number and buildstamp are expected to be in files {@code foo.version} and 
+ *  {@code foo.build} respectively. The buildstamp may be omitted, in which case
+ *  it assumes a set default value.
+ */
 public final class AppVersion {
-  interface DefaultValueSupplier {
+  /**
+   *  Provides the default value for a buildstamp, when the latter is missing
+   *  from the classpath.
+   */
+  @FunctionalInterface
+  public interface DefaultValueSupplier {
     String get() throws IOException;
   }
   
-  static final class Constant implements DefaultValueSupplier {
+  /**
+   *  A default value represented as a string constant.
+   */
+  public static final class Constant implements DefaultValueSupplier {
     private final String def;
     
     Constant(String def) { this.def = def; }
@@ -21,18 +38,42 @@ public final class AppVersion {
   
   private AppVersion() {}
   
+  /**
+   *  This variant of {@link #get} obtains a concatenation of the application's version number
+   *  and buildstamp, deferring to the constant {@code 0} if the optional buildstamp file is missing.
+   *  
+   *  @param appName The app name.
+   *  @return The concatenated string.
+   *  @throws IOException If an I/O error occurs.
+   */
   public static String get(String appName) throws IOException {
-    return getFile(appName + ".version") + "_" + getFile(appName + ".build", new Constant("0"));
+    return get(appName, new Constant("0"));
+  }
+  
+  /**
+   *  Obtains a concatenation of the application's version number and buildstamp, in the form
+   *  {@code version_build}. The version file <em>must</em> be present in the root classpath, and be
+   *  named {@code appName.version}. The buildstamp file {@code appName.build} is optional; if
+   *  the file is not found, the value returned by the {@code buildstampDefaultValueSupplier}
+   *  is used in its place.
+   *  
+   *  @param appName The app name.
+   *  @param buildstampDefaultValueSupplier Used if the buildstamp file is not found.
+   *  @return The concatenated string.
+   *  @throws IOException If an I/O error occurs.
+   */
+  public static String get(String appName, DefaultValueSupplier buildstampDefaultValueSupplier) throws IOException {
+    return getFile(appName + ".version") + "_" + getFile(appName + ".build", buildstampDefaultValueSupplier);
   }
     
-  static String getFile(String versionFile) throws IOException {
-    return getFile(versionFile, () -> {
-      throw new FileNotFoundException("Not found: " + versionFile);
+  static String getFile(String file) throws IOException {
+    return getFile(file, () -> {
+      throw new FileNotFoundException("Not found: " + file);
     });
   }
   
-  static String getFile(String versionFile, DefaultValueSupplier defaultValueSupplier) throws IOException {
-    return readResourceHead(versionFile, defaultValueSupplier);
+  static String getFile(String file, DefaultValueSupplier defaultValueSupplier) throws IOException {
+    return readResourceHead(file, defaultValueSupplier);
   }
   
   private static String readResourceHead(String file, DefaultValueSupplier defaultValueSupplier) throws IOException {
