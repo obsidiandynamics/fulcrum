f450d6b6f4a88b6c153e47bb9bf4f3e736411a1e
Emil Koutanov <ekoutanov@gmail.com>
2018-04-01 17:36:09 +1000

    Benchmarking with Slf4j

--- zlg/src/test/java/com/obsidiandynamics/zlg/LogDisabledVolumeTest.java
+++ zlg/src/test/java/com/obsidiandynamics/zlg/LogDisabledVolumeTest.java
@@ -1,10 +1,15 @@
 package com.obsidiandynamics.zlg;
 
+import static org.junit.Assert.*;
+
 import org.junit.*;
 import org.junit.runner.*;
 import org.junit.runners.*;
+import org.slf4j.*;
 
 import com.obsidiandynamics.testmark.*;
+
+import squidpony.squidmath.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public final class LogDisabledVolumeTest {
@@ -13,7 +18,7 @@ public final class LogDisabledVolumeTest {
   
   @FunctionalInterface
   private interface TestCycle {
-    void cycle();
+    void cycle(float randomFloat, double randomDouble, int randomInt, long randomLong);
   }
 
   @Test
@@ -23,7 +28,19 @@ public final class LogDisabledVolumeTest {
 
   @Test
   public void testBaselineBenchmark() {
-    Testmark.ifEnabled(() -> runBenchmark("Baseline", BENCHMARK_RUN_TIME_NANOS, nopCycle()));
+    final String name = "Baseline";
+    Testmark.ifEnabled(name, () -> runBenchmark(name, BENCHMARK_RUN_TIME_NANOS, nopCycle()));
+  }
+  
+  @Test
+  public void testSlf4j() {
+    runBenchmark(null, TEST_RUN_TIME_NANOS, slf4jCycle());
+  }
+  
+  @Test
+  public void testSlf4jBenchmark() {
+    final String name = "Slf4j";
+    Testmark.ifEnabled(name, () -> runBenchmark(name, BENCHMARK_RUN_TIME_NANOS, slf4jCycle()));
   }
   
   @Test
@@ -33,21 +50,43 @@ public final class LogDisabledVolumeTest {
   
   @Test
   public void testZlgBenchmark() {
-    Testmark.ifEnabled(() -> runBenchmark("Zlg", BENCHMARK_RUN_TIME_NANOS, zlgCycle()));
+    final String name = "Zlg";
+    Testmark.ifEnabled(name, () -> runBenchmark(name, BENCHMARK_RUN_TIME_NANOS, zlgCycle()));
   }
   
   private static void runBenchmark(String name, long runTimeNanos, TestCycle cycle) {
+    final long warmupTime = runTimeNanos / 2;
     final long checkRuns = 1_000_000L;
-    final long started = System.nanoTime();
+    boolean warmup = true;
     
     long cycles = 0;
     long took;
+    final ThrustRNG random = new ThrustRNG();
+    if (name != null) System.out.print("Warming up... ");
+    long started = System.nanoTime();
     for (;;) {
-      cycle.cycle();
+      // generate random numbers to avoid cache hits for boxed primitives
+      final double randomDouble = RandomFP.toDouble(random.nextLong());
+      final int randomInt = (int) (Integer.MAX_VALUE * randomDouble);
+      final long randomLong = randomInt;
+      final float randomFloat = (float) randomDouble;
+      
+      cycle.cycle(randomFloat, randomDouble, randomInt, randomLong);
       cycles++;
       if (cycles % checkRuns == 0) {
         took = System.nanoTime() - started;
-        if (took > runTimeNanos) break;
+        if (warmup) {
+          if (took > warmupTime) {
+            warmup = false;
+            if (name != null) System.out.println("starting timed run... ");
+            cycles = 0;
+            started = System.nanoTime();
+          }
+        } else {
+          if (took > runTimeNanos) {
+            break;
+          }
+        }
       }
     }
     
@@ -58,13 +97,29 @@ public final class LogDisabledVolumeTest {
   }
   
   private static TestCycle nopCycle() {
-    return () -> {};
+    return (f, d, i, l) -> {
+      assert f >= 0;
+      assert d >= 0;
+      assert i >= 0;
+      assert l >= 0;
+    };
   }
 
   private static TestCycle zlgCycle() {
     final Zlg z = Zlg.forClass(LogDisabledVolumeTest.class).get();
-    return () -> {
-      z.t("Int %d, float %f, boolean %b, long %d").arg(100).arg(0.5).arg(false).arg(400L).log();
+    assertFalse(z.isEnabled(LogLevel.TRACE));
+    
+    return (f, d, i, l) -> {
+      z.t("float: %f, double: %f, int: %d, long: %d").arg(f).arg(d).arg(i).arg(l).log();
+    };
+  }
+
+  private static TestCycle slf4jCycle() {
+    final Logger logger = LoggerFactory.getLogger(LogDisabledVolumeTest.class);
+    assertFalse(logger.isTraceEnabled());
+    
+    return (f, d, i, l) -> {
+      logger.trace("float: {}, double: {}, int: {}, long: {}", f, d, i, l);
     };
   }
   
