b31bf894e7f315371324884f3b1be67856a87331
Emil Koutanov <ekoutanov@gmail.com>
2018-04-04 10:29:04 +1000

    Renamed variable

--- zlg/src/test/java/com/obsidiandynamics/zlg/sample/SysOutLoggingSample.java
+++ zlg/src/test/java/com/obsidiandynamics/zlg/sample/SysOutLoggingSample.java
@@ -5,10 +5,10 @@ import java.util.*;
 import com.obsidiandynamics.zlg.*;
 
 public final class SysOutLoggingSample {
-  private static final Zlg z = Zlg.forClass(SysOutLoggingSample.class).get();
+  private static final Zlg zlg = Zlg.forClass(SysOutLoggingSample.class).get();
   
   public static void main(String[] args) {
-    z.i("Starting with %d args: %s").arg(args.length).arg(Arrays.asList(args)).log();
-    z.w("An error occurred at %s").arg(new Date()).stack(new RuntimeException()).tag("I/O").log();
+    zlg.i("Starting with %d args: %s").arg(args.length).arg(Arrays.asList(args)).log();
+    zlg.w("An error occurred at %s").arg(new Date()).stack(new RuntimeException()).tag("I/O").log();
   }
 }
