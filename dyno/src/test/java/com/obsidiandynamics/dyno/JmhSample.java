ea5ac9e213990bcc66db0a3b5ecd81e45daade15
Emil Koutanov <ekoutanov@gmail.com>
2018-04-06 08:49:15 +1000

    Updated sample

--- dyno/src/test/java/com/obsidiandynamics/dyno/JmhSample.java
+++ dyno/src/test/java/com/obsidiandynamics/dyno/JmhSample.java
@@ -1,6 +1,7 @@
 package com.obsidiandynamics.dyno;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.options.*;
 
 public class JmhSample implements BenchmarkTarget {
   @Override
@@ -12,8 +13,11 @@ public class JmhSample implements BenchmarkTarget {
     new Dyno()
     .withBenchTime(5_000)
     .withTarget(JmhSample.class)
+    .withThreads(2)
     .withDriver(new JmhDriver(opts -> opts
                               .mode(Mode.Throughput)
+                              .verbosity(VerboseMode.EXTRA)
+                              .forks(1)
                               .measurementIterations(2)))
     .withWarmupFraction(0.1)
     .withOutput(System.out::println)
